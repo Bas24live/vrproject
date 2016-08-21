5033620ee0be56117f9b9bcf28d326d5230290c8
Bas24live <bas24.7live@gmail.com>
2016-08-22 00:46:15 +0200

    Added player movement, movable walls and a working camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     private Rigidbody rb;
+    public float speed;
 
     // Use this for initialization
     void Start () {
@@ -12,19 +13,24 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        // Rotate 90 degrees clockwise
+        if (Input.GetKeyDown("d"))
+            transform.Rotate(0, 90, 0, Space.Self);
+
+        // Rotate 90 degrees anticlockwise
+        if (Input.GetKeyDown("a"))
+            transform.Rotate(0, -90, 0, Space.Self);
+       
+    }
 
     // Physics calculations
     void FixedUpdate ()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);        
 
-        rb.AddForce(movement);
-        
-
+        transform.Translate(movement * speed);        
     }
 }
