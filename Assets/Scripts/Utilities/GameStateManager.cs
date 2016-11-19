15a8c77781b5bbddcaf132da59c479890852d65f
Minnaar-Colin Fullard <mcfullard@gmail.com>
2016-11-19 16:16:19 +0200

    Added unity files

--- Assets/Scripts/Utilities/GameStateManager.cs
+++ Assets/Scripts/Utilities/GameStateManager.cs
@@ -5,14 +5,64 @@ public class GameStateManager : MonoBehaviour {
 
     public Transform canvas;
     public Transform playerCamera;
+    private Transform mapParent;
+    private Transform dynBlockPrefab;
 
     bool paused;
 
-    void Start() {
-        paused = false;        
+    void Awake()
+    {
+        dynBlockPrefab = GameObject.FindWithTag("Wall_Inner").transform;
+        DungeonGenerator.instance.GenerateHauberkDungeon(23, 23);
+        GenerateByGrid(DungeonGenerator._dungeon);
     }
 
-	void Update () {
+    void Start() {
+        paused = false;
+    }
+
+    public void GenerateByGrid(Tile[,] grid)
+    {
+        int xSize = grid.GetLength(0);
+        int ySize = grid.GetLength(1);
+        int xOff = 1;
+        int yOff = 0;
+        int zOff = 1;
+
+        Vector3 midOff = new Vector3(0F, -yOff / 2F, 0F); 
+        mapParent = new GameObject().transform;
+        mapParent.name = "DynamicBlocks";
+
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                switch (grid[x, y])
+                {
+                    default:
+                        break;
+                    case Tile.Floor:
+                        //CreateBlock(new Vector3(xOff * x, yOff * 0F, zOff * y) + midOff);
+                        break;
+                    case Tile.RoomFloor:
+                        //CreateBlock(new Vector3(xOff * x, yOff * 0F, zOff * y) + midOff);
+                        break;
+                    case Tile.Wall:
+                        CreateBlock(new Vector3(xOff * x, yOff * 0F, zOff * y) + midOff);
+                        CreateBlock(new Vector3(xOff * x, yOff * 1F, zOff * y) + midOff);
+                        break;
+                }
+            }
+        }
+    }
+
+    private void CreateBlock(Vector3 pos)
+    {
+        GameObject block = Instantiate(dynBlockPrefab, pos, Quaternion.identity) as GameObject;
+        block.transform.parent = mapParent;
+    }
+
+    void Update () {
         if (Input.GetKeyDown(KeyCode.Escape))
             paused = !paused;
 
