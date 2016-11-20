5d0a3da42d88aaf59eb8fb930ff31f508892a700
Bas24live <bas24.7live@gmail.com>
2016-11-20 10:14:46 +0200

    NavMesh
    NavMesh now baking when map is generated.

--- Assets/Scripts/Utilities/GameStateManager.cs
+++ Assets/Scripts/Utilities/GameStateManager.cs
@@ -15,8 +15,7 @@ public class GameStateManager : MonoBehaviour {
 
     bool paused;
 
-    void Awake()
-    {   
+    void Awake() {   
         worldContainer = GameObject.FindGameObjectWithTag("World");
         playerCamera = GameObject.FindGameObjectWithTag("PlayerCamera");
     }
@@ -38,8 +37,7 @@ public class GameStateManager : MonoBehaviour {
         wallsContainer = new GameObject("Walls");
         wallsContainer.transform.SetParent(worldContainer.transform);
 
-        GameObject floor = Instantiate(floorPrefab, new Vector3((xSize - 1)/2, 0, (ySize - 1)/2), Quaternion.identity, wallsContainer.transform) as GameObject;
-        floor.transform.localScale += new Vector3(.1f * (xSize/2), 0, .1f * (ySize/2));
+        GenerateFloor(xSize, ySize);
 
         for (int x = 0; x < xSize; x++)
             for (int y = 0; y < ySize; y++)
@@ -47,12 +45,17 @@ public class GameStateManager : MonoBehaviour {
                     CreateBlock(new Vector3(xOff * x, yOff, zOff * y));
     }
 
-    private void CreateBlock(Vector3 pos)
+    void CreateBlock(Vector3 pos)
     {
         Instantiate(blockPrefab, pos, Quaternion.identity, wallsContainer.transform);
     }
 
+    void GenerateFloor(float width, float height) {
+        GameObject floor = Instantiate(floorPrefab, new Vector3((width - 1) / 2, 0, (height - 1) / 2), Quaternion.identity, wallsContainer.transform) as GameObject;
+        floor.transform.localScale += new Vector3(.1f * (width / 2.5f), 0, .1f * (height / 2.5f));
 
+        UnityEditor.NavMeshBuilder.BuildNavMesh();
+    }
 
     void Update () {
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -62,23 +65,16 @@ public class GameStateManager : MonoBehaviour {
             else
                 Resume();
         }
-            
-
-        
 	}
 
     public void Resume() {
         paused = false;
         Time.timeScale = 1;        
-        pauseMenu.gameObject.SetActive(false);
-        
+        pauseMenu.gameObject.SetActive(false);  
     }
 
     public void Pause() {
         Time.timeScale = 0;
         pauseMenu.gameObject.SetActive(true);
-        
     }
-
-
 }
