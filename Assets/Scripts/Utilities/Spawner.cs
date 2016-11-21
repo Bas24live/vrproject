b00166819ccc09133a07a50ce83634f8a21b097e
Bas24live <bas24.7live@gmail.com>
2016-11-21 15:06:27 +0200

    Added Spawner Script Skeleton

---
+++ Assets/Scripts/Utilities/Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+
+
+
+
+
+    void SpawnGameObject(GameObject gameObject, Transform parent) {
+        //Spawn gameobject at an open spwan location, use that as the position for this gameobject
+    }
+
+    void SpawnGameObject(Vector3 position, GameObject gameObject, Transform parent) {
+        Instantiate(gameObject, position, gameObject.transform.rotation, parent);
+    }
+}
